fc9a4f97f707c6643624a49cb8d5748110e098ef
iswilljr <iswilljr@gmail.com>
2023-06-11 13:32:06 -0500

    add download playlist dialog

---
+++ src/components/download.tsx
@@ -0,0 +1,88 @@
+'use client'
+
+import { useMemo, useState } from 'react'
+import { Button } from '@/ui/button'
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from '@/ui/dialog'
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/ui/select'
+import type { DialogProps } from '@radix-ui/react-dialog'
+import type { Manifest, Playlist } from 'm3u8-parser'
+
+export interface ParsedData {
+  downloadable: boolean
+  manifest: Manifest
+}
+
+export interface ExtractedData {
+  playlists: string[]
+  info: ParsedData[]
+}
+
+const getPlaylistName = (playlist?: Playlist) => {
+  const { NAME: name, RESOLUTION: resolution } = playlist?.attributes ?? {}
+
+  return name ?? (resolution ? `${resolution.width}x${resolution?.height}` : 'video')
+}
+
+export function DownloadDialog({
+  extractedData,
+  parsedData,
+  ...props
+}: DialogProps & { parsedData?: ParsedData; extractedData?: ExtractedData }) {
+  const [selectedPlaylist, setSelectedPlaylist] = useState('0')
+  const [selectedPlaylistFormat, setSelectedPlaylistFormat] = useState('0')
+
+  const playlists = useMemo(() => {
+    const data = parsedData ?? extractedData?.info?.at(+selectedPlaylist)
+
+    return !data?.downloadable
+      ? data?.manifest.playlists
+      : [{ uri: extractedData?.playlists.at(+selectedPlaylist) ?? '', attributes: { NAME: 'unknown' } }]
+  }, [extractedData, parsedData, selectedPlaylist])
+
+  return (
+    <Dialog {...props}>
+      <DialogContent className='max-w-none'>
+        <DialogHeader>
+          <DialogTitle>Download Playlist Options</DialogTitle>
+        </DialogHeader>
+        <div className='space-y-1 dark:border-neutral-800'>
+          {extractedData && (
+            <>
+              <label className='text-sm' htmlFor='playlist'>
+                Playlist:
+              </label>
+              <Select value={selectedPlaylist} onValueChange={setSelectedPlaylist}>
+                <SelectTrigger className='w-full' id='playlist'>
+                  {`Playlist ${+selectedPlaylist + 1}`}
+                </SelectTrigger>
+                <SelectContent>
+                  {extractedData?.playlists?.map((playlist, index) => (
+                    <SelectItem className='max-w-xs break-all sm:max-w-md' key={playlist} value={`${index}`}>
+                      {new URL(playlist).pathname}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+            </>
+          )}
+          <label className='text-sm' htmlFor='format'>
+            Format:
+          </label>
+          <Select value={selectedPlaylistFormat} onValueChange={setSelectedPlaylistFormat}>
+            <SelectTrigger className='w-full' id='format'>
+              <SelectValue placeholder='Format' />
+            </SelectTrigger>
+            <SelectContent>
+              {playlists?.map((playlist, index) => (
+                <SelectItem key={index} value={`${index}`}>
+                  {getPlaylistName(playlist)}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+        </div>
+        <Button type='button'>Download</Button>
+      </DialogContent>
+    </Dialog>
+  )
+}
--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,16 +1,104 @@
 'use client'
 
+import axios from 'redaxios'
+import useSWRMutation from 'swr/mutation'
+import { z } from 'zod'
+import { useMemo, useState } from 'react'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
 import { Button } from '@/ui/button'
+import { DownloadDialog, type ExtractedData, type ParsedData } from './download'
+
+interface ArgOptions {
+  arg: string
+}
+
+const form = z.object({
+  url: z.string().url('Enter a valid url'),
+})
+
+async function requestEndpoint<T>(path: string, { arg }: ArgOptions) {
+  const res = await axios.get<T>(`/${path}?url=${arg}`)
+  return res.data
+}
 
 export function Form() {
+  const [open, setOpen] = useState(false)
+
+  const {
+    trigger: parseUrl,
+    data: parsedUrlData,
+    error: parsingError,
+    isMutating: isParsing,
+    reset: resetParsedData,
+  } = useSWRMutation('api/parse', requestEndpoint<ParsedData>)
+
+  const {
+    trigger: extractUrl,
+    data: extractedData,
+    error: extractingError,
+    isMutating: isExtracting,
+    reset: resetExtractedData,
+  } = useSWRMutation('api/extract', requestEndpoint<ExtractedData>)
+
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      url: 'https://fast.wistia.com/embed/medias/agj9rjnxa4.m3u8',
+    },
+    mode: 'onSubmit',
+    resolver: zodResolver(form),
+  })
+
+  const error = useMemo(
+    () => errors.url?.message ?? extractingError?.data?.message ?? parsingError?.data?.message,
+    [errors?.url?.message, extractingError?.data?.message, parsingError?.data?.message]
+  )
+
   return (
-    <form className='flex items-center'>
-      <input
-        className='bg-secondary-8/80 focus:border-primary-6 mt-2 w-full rounded-l-full border-none bg-neutral-600/50 px-4 py-2 text-gray-300 outline-none ring-1 ring-neutral-700 duration-150 placeholder:text-neutral-500  focus:ring-2 hover:[&:not(:focus)]:ring-neutral-600'
-        id='url'
-        placeholder='https://example.com/my/playlist.m3u8'
-      />
-      <Button className='border-l-none !mt-2 rounded-full rounded-l-none'>Convert</Button>
+    <form
+      onSubmit={handleSubmit(async values => {
+        resetParsedData()
+        resetExtractedData()
+
+        const url = new URL(values.url)
+
+        if (!url.protocol.startsWith('http')) {
+          return setError('url', {
+            message: 'Enter a valid url',
+          })
+        }
+
+        const isM3U8File = url.pathname.replace(/\/+$/, '').endsWith('.m3u8')
+
+        ;(isM3U8File ? parseUrl(values.url) : extractUrl(values.url)).then(() => setOpen(true)).catch(() => null)
+      })}
+    >
+      <div className='flex items-center'>
+        <input
+          className='bg-secondary-8/80 focus:border-primary-6 mr-0.5 mt-2 w-full rounded-l-full border-none bg-neutral-600/50 px-4 py-2 text-gray-300 outline-none ring-1 ring-neutral-700 duration-150  placeholder:text-neutral-500 focus:ring-2 hover:[&:not(:focus)]:ring-neutral-600'
+          id='url'
+          placeholder='https://example.com/my/playlist.m3u8'
+          {...register('url')}
+        />
+        <Button
+          className='border-l-none mt-2 rounded-full rounded-l-none'
+          type='submit'
+          loading={isParsing || isExtracting}
+        >
+          Convert
+        </Button>
+      </div>
+      {error && (
+        <div className='mt-2'>
+          <p className='text-sm text-red-500'>{error}</p>
+        </div>
+      )}
+      <DownloadDialog parsedData={parsedUrlData} extractedData={extractedData} open={open} onOpenChange={setOpen} />
     </form>
   )
 }
