19f774ed311ee93258da9ed21f8af92a13675bed
iswilljr <iswilljr@gmail.com>
2023-06-11 13:32:58 -0500

    improve button component design

--- src/ui/button.tsx
+++ src/ui/button.tsx
@@ -1,9 +1,11 @@
+/* eslint-disable @typescript-eslint/prefer-nullish-coalescing */
 import * as React from 'react'
+import { TbLoader } from 'react-icons/tb'
 import { cva, type VariantProps } from 'class-variance-authority'
 import { cx } from '@/utils/cx'
 
 const buttonVariants = cva(
-  'flex items-center justify-center rounded-md ring-1 px-4 py-2 font-medium transition-all duration-200 outline-none ease-in-out focus-visible:ring-2 disabled:pointer-events-none disabled:opacity-50 active:scale-95',
+  'flex items-center justify-center rounded-md ring-1 px-4 py-2 font-medium transition-all duration-200 outline-none ease-in-out focus-visible:ring-2 disabled:pointer-events-none disabled:opacity-50',
   {
     variants: {
       variant: {
@@ -21,13 +23,21 @@ export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
   icon?: React.ReactNode
+  loading?: boolean
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ children, className, icon, variant, ...props }, ref) => {
+  ({ children, className, icon, loading, variant, ...props }, ref) => {
     return (
-      <button ref={ref} className={cx(buttonVariants({ variant, className }))} {...props}>
-        {icon && <span className={cx(children && 'mr-2')}>{icon}</span>}
+      <button
+        ref={ref}
+        disabled={loading || props.disabled}
+        className={cx(buttonVariants({ variant, className }))}
+        {...props}
+      >
+        {(loading || icon) && (
+          <span className={cx(children && 'mr-2')}>{loading ? <TbLoader className='animate-spin' /> : icon}</span>
+        )}
         {children}
       </button>
     )
