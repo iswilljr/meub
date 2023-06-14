5fb1a2b27dad325ac1a5ef77e095b67d07e75143
iswilljr <iswilljr@gmail.com>
2023-06-14 15:57:18 -0500

    transform playlists and segments uri

--- src/utils/parse-m3u8.ts
+++ src/utils/parse-m3u8.ts
@@ -1,4 +1,5 @@
 import { type Manifest, Parser } from 'm3u8-parser'
+import { z } from 'zod'
 
 function isManifestDownloadable(manifest: Manifest) {
   return (manifest.segments?.length ?? 0) > 0
@@ -22,9 +23,29 @@ export async function parseM3U8(url: string) {
     downloadable: isManifestDownloadable(parser.manifest),
     valid: isValidManifest(parser.manifest),
     manifest: {
-      playlists: parser.manifest.playlists,
-      segments: parser.manifest.segments,
+      playlists:
+        parser.manifest.playlists?.map(playlist => ({
+          attributes: playlist.attributes,
+          uri: parseUri(url, playlist.uri),
+        })) ?? [],
+      segments:
+        parser.manifest.segments?.map(segment => ({
+          uri: parseUri(url, segment.uri),
+        })) ?? [],
       targetDuration: parser.manifest.targetDuration,
     },
   }
+}
+
+function parseUri(base: string, uri: string) {
+  const isUrl = z.string().url().safeParse(uri).success
+
+  if (isUrl) return uri
+
+  const baseURL = new URL(base)
+  const basePathname = baseURL.pathname.replace(/\/+$/, '').split('/').slice(0, -1).join('/')
+  const pathname = `${basePathname}/${uri}`
+  const parsedURL = new URL(pathname, baseURL)
+
+  return parsedURL.toString()
 }
